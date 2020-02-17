cb63df6158101231f41e0db4e3f0d9768767bbf8
haryel <haryel_hca@hotmail.com>
2020-02-16 21:40:48 -0300

    Add project files.

---
+++ API/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("ApiCorsPolicy")]
+    public class LoginController : ControllerBase
+    {
+        public bool Post([FromBody] User user)
+        {
+            return true;
+        }
+    }
+
+    public class User
+    {
+        public string usuario { get; set; }
+        public string senha { get; set; }
+    }
+}
